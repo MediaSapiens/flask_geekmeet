f5baea5bde42e00e8807bd6e0ab9d9e8a36c4a40
sergej felde <sfelde@gmail.com>
2011-11-23 12:11:04 +0100

    add 404 page handling

--- pages.py
+++ pages.py
@@ -10,5 +10,8 @@ def flask_geekmeet(page=None):
     except TemplateNotFound:
         abort(404)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 if __name__ == '__main__':
     app.run()
